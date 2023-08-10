c94ba21178a9bae584ea988a773cae59b1798dfb
samad <sampv0166@gmail.com>
2023-08-10 20:34:16 +0530

    added leaderboard and chat layout

--- frontend/src/app/(app)/chat/page.tsx
+++ frontend/src/app/(app)/chat/page.tsx
@@ -1,9 +1,21 @@
-"use client"
-import React from 'react'
+import React from 'react';
+
 export default function Chat() {
   return (
-    <>
-    <h1>Chat Page</h1>
-    </>
-  )
+    <div className="flex h-screen w-full bg-black">
+      <div className="flex flex-col">
+        <div className="border border-red-500 h-full">
+          <p className="text-white">left container for channels / messages</p>
+        </div>
+
+        <div className="border border-red-500 h-full">
+          <p className="text-white">Friends search list</p>
+        </div>
+      </div>
+
+      <div className="w-full border border-green-500">
+        <p className="text-white">Chat Window</p>
+      </div>
+    </div>
+  );
 }
--- frontend/src/app/(app)/leaderboard/page.tsx
+++ frontend/src/app/(app)/leaderboard/page.tsx
@@ -1,7 +1,103 @@
-import React from 'react'
+import React from 'react';
 
 export default function Leaderboard() {
   return (
-    <h1>Leaderboard</h1>
-  )
+    <div className="container mx-auto p-6">
+      <div className="mb-6 text-center">
+        <h1 className="text-2xl font-bold mb-4">
+          Top Player Heading With Icon
+        </h1>
+      </div>
+      <div className="mb-6 w-full flex justify-center items-center">
+        <div className="flex space-x-20">
+          <div className="flex items-center space-x-4 relative flex-1">
+            <div className="w-12 h-12 rounded-full bg-gray-300 flex-shrink-0 relative">
+              {/* Avatar Image */}
+              <div className="absolute w-4 h-4 bg-red-500 rounded-full right-0 bottom-0"></div>
+            </div>
+            <div>
+              <p className="font-semibold">Player 1</p>
+              <p className="font-semibold">1900</p>
+            </div>
+          </div>
+          <div className="flex items-center space-x-4 relative flex-1">
+            <div className="w-12 h-12 rounded-full bg-gray-300 flex-shrink-0 relative">
+              {/* Avatar Image */}
+              <div className="absolute w-4 h-4 bg-red-500 rounded-full right-0 bottom-0"></div>
+            </div>
+            <div>
+              <p className="font-semibold">Player 1</p>
+              <p className="font-semibold">1900</p>
+            </div>
+          </div>
+          <div className="flex items-center space-x-4 relative flex-1">
+            <div className="w-12 h-12 rounded-full bg-gray-300 flex-shrink-0 relative">
+              {/* Avatar Image */}
+              <div className="absolute w-4 h-4 bg-red-500 rounded-full right-0 bottom-0"></div>
+            </div>
+            <div>
+              <p className="font-semibold">Player 1</p>
+              <p className="font-semibold">1900</p>
+            </div>
+          </div>
+          {/* Repeat for other players */}
+        </div>
+        {/* Repeat for other players */}
+      </div>
+      <h2 className="text-xl font-bold mb-4 text-center mt-10">
+        Leaderboard Heading with icon{' '}
+      </h2>
+      <table className="w-full bg-black text-white border border-white">
+        <thead className="border border-white">
+          <tr>
+            <th className="py-2 px-4 text-center bg-black text-white shadow-md">
+              Player
+            </th>
+            <th className="py-2 px-4 text-center bg-black text-white shadow-md">
+              Rank
+            </th>
+            <th className="py-2 px-4 text-center bg-black text-white shadow-md">
+              Score
+            </th>
+            <th className="py-2 px-4 text-center bg-black text-white shadow-md">
+              Games
+            </th>
+            <th className="py-2 px-4 text-center bg-black text-white shadow-md">
+              Wins
+            </th>
+            <th className="py-2 px-4 text-center bg-black text-white shadow-md">
+              Losses
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="py-2 px-4 text-center">Player 1</td>
+            <td className="py-2 px-4 text-center">1</td>
+            <td className="py-2 px-4 text-center">1900</td>
+            <td className="py-2 px-4 text-center">10</td>
+            <td className="py-2 px-4 text-center">7</td>
+            <td className="py-2 px-4 text-center">3</td>
+          </tr>
+          <tr>
+            <td className="py-2 px-4 text-center">Player 1</td>
+            <td className="py-2 px-4 text-center">1</td>
+            <td className="py-2 px-4 text-center">1900</td>
+            <td className="py-2 px-4 text-center">10</td>
+            <td className="py-2 px-4 text-center">7</td>
+            <td className="py-2 px-4 text-center">3</td>
+          </tr>
+          <tr>
+            <td className="py-2 px-4 text-center">Player 1</td>
+            <td className="py-2 px-4 text-center">1</td>
+            <td className="py-2 px-4 text-center">1900</td>
+            <td className="py-2 px-4 text-center">10</td>
+            <td className="py-2 px-4 text-center">7</td>
+            <td className="py-2 px-4 text-center">3</td>
+          </tr>
+          {/* Repeat for other players */}
+        </tbody>
+      </table>
+    </div>
+  );
 }
