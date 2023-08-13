9b27fbd9f4cd2012cf56dd46647dc652dfe2c87b
Ghaith Abdoush <ghaith181994@hotmail.com>
2023-08-13 16:38:06 +0400

    Fix the Auth btn in Login page

--- frontend/src/components/AuthButton/IntraAuth.tsx
+++ frontend/src/components/AuthButton/IntraAuth.tsx
@@ -1,27 +1,18 @@
 import Image from "next/image";
 
-
-/*
-* TODO: # On width of 780px:
-*           1- Change the size of the button
-* 		    2- Change the font size
-* */
 export function IntraAuthButton() {
 	return (
 		<>
-			<div className="w-[400px] h-[80px] rounded-md shadow border border-zinc-800
-							bg-gradient-to-b from-authGrad-s to-authGrad-e
-							hover:bg-gradient-to-b hover:from-hover-authGrad-s hover:to-hover-authGrad-e hover:shadow-lg hover:border-main-yellow
-							flex-row justify-center items-center gap-5 inline-flex text-[30px] font-bold hover:cursor-pointer">
+			<div className="w-56 md:w-96 h-20 md:h-28 rounded-md
+							flex-col md:flex-row justify-center items-center gap-5 inline-flex 
+							text-2xl md:text-3xl font-bold
+							shadow border border-zinc-800 bg-gradient-to-b from-authGrad-s to-authGrad-e
+							hover:bg-gradient-to-b hover:from-hover-authGrad-s hover:to-hover-authGrad-e hover:shadow-lg hover:border-main-yellow hover:cursor-pointer">
 				<div className="font-saira-condensed left-0 text-center text-main-yellow">
 					Authenticate 42 Intra
 				</div>
-				<div>
-					<Image alt="auth-btn-separator" src="separator.svg" width={5} height={5}/>
-				</div>
-				<div className="font-saira-condensed w-[50px] h-11 text-center text-main-text">
-					42
-				</div>
+				<div className="hidden md:block"><Image alt="auth-btn-separator" src="separator.svg" width={5} height={5}/></div>
+				<div className="hidden md:block font-saira-condensed w-[50px] h-11 text-center text-main-text">42</div>
 			</div>
 		</>
 	);
