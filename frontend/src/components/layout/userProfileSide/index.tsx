384232ecc564955960c265d8584923289de2ec5a
yonatan46 <yonatankefyalew65@gmail.com>
2023-08-14 19:57:02 +0400

    updated avatar status (bottom) 🟢

--- frontend/src/components/layout/userProfileSide/index.tsx
+++ frontend/src/components/layout/userProfileSide/index.tsx
@@ -6,16 +6,14 @@ export default function UserProfileSide() {
 	return (
 		<>
 			<div className="flex flex-row items-center justify-between px-[50px] pt-8 pb-4 ">
-				<div className="avatar online ">
-					<div className="w-[66px] h-[66px] rounded-full">
-						<img src="/av1.svg" />
-					</div>
+
+				<div className="relative">
+					<img className="w-24 h-24 rounded-full" src="/av1.svg" />
+					<span className="bottom-1 left-16  absolute  w-3.5 h-3.5 bg-green-400 border-2 border-white dark:border-gray-800 rounded-full"></span>
 				</div>
+
 				<div className="text-main-text font-saira-condensed font-bold text-xl">Gab-172</div>
 			</div>
-
-
-			{/* braker to be updated */}
 			<Breaker />
 			<StartGameCustomize />
 		</>
