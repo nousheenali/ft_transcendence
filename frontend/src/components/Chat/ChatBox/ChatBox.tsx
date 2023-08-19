1ceac807892b923b1d7e695e01891cec4c4571bc
Gab-182 <ghaith181994@hotmail.com>
2023-08-19 14:17:58 +0400

    trying to use the props from getStaticProps()

--- frontend/src/components/Chat/ChatBox/ChatBox.tsx
+++ frontend/src/components/Chat/ChatBox/ChatBox.tsx
@@ -1,12 +1,9 @@
-"use client";
-
-import React, { useState } from "react";
-import Image from "next/image";
+import ChatBoxHeader from "./ChatBoxHeader/ChatBoxHeader";
 
 export default function ChatBox() {
   return (
     <div className="w-full h-20 flex justify-between rounded-xl bg-main-theme text-main-texts font-saira-condensed border-b border-main-yellow">
-
+      <ChatBoxHeader />
     
     </div>
   );
--- frontend/src/components/Chat/LatestMsgsBox/LatestMsgsBox.tsx
+++ frontend/src/components/Chat/LatestMsgsBox/LatestMsgsBox.tsx
@@ -1,5 +1,4 @@
 import LatestMessage from "@/components/Chat/LatestMsgsBox/LatestMessage/LatestMessage";
-import { messages } from "./message_example";
 
 /**
  * A component that renders the LatestMsgsBox component, which is a box that contains the latest messages
@@ -18,12 +17,13 @@ import { messages } from "./message_example";
  * {messageTime} is a string that represents the time of the message
  * {messageContent} is a string that represents the content of the message
  * 
- * The messages array is imported from the message_example.ts file (frontend/src/components/Chat/LatestMsgsBox/message_example.ts)
  */
-export default function LatestMsgsBox() {
+export default function LatestMsgsBox(props: {message:string}) {
+  const {message} = props;
+
   return (
     <div className="flex flex-col w-full h-1/2 rounded-xl pl-5 overflow-y-scroll scroll-container">
-      {messages.map((message, index) => (
+      {/* {messages.map((message:any, index:number) => (
         <div className="py-1">
           <LatestMessage
             key={index}
@@ -32,7 +32,42 @@ export default function LatestMsgsBox() {
             messageContent={message.messageContent}
           />
         </div>
-      ))}
+      ))} */}
     </div>
   );
+}
+
+/**
+ * Example of a message object:
+ * ----------------------------
+ * 	sender: {
+		name: "Ghaiath Abdoush",
+		profileImage: {
+		  alt: "Ghaiath Abdoush Profile Picture",
+		  src: "av1.svg",
+		},
+		isOnline: false,
+	  },
+	  messageTime: "2m ago",
+	  messageContent: "Lorem Ipsum 1",
+	},
+ */
+
+
+  /**
+   * The getStaticProps function is used to fetch the data that is needed to render the LatestMsgsBox component,
+   * it is used to fetch the messages array from the database.
+   */
+
+
+  export async function getStaticProps() {
+    const {messages} = await import("../../../data/messages.json");
+
+    console.log(messages);
+
+    return {
+        props: {
+            messages: messages,
+        },
+    }
 }
