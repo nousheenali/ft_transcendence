433f4a2e5180021b4587dc1c8d074199476eb0b6
yonatan46 <yonatankefyalew65@gmail.com>
2023-09-13 16:45:55 +0400

    rename file names to channel

--- backend/src/channel/channel.service.ts
+++ backend/src/channel/channel.service.ts
@@ -2,9 +2,22 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service'; // 👈 Import PrismaService 
 
 @Injectable()
-export class CreateChannelService {
+export class ChannelService {
   constructor(private prisma: PrismaService) { } // 👈 Inject PrismaService class into the constructor.
   async createChannel(CreateChannel) {
+    const find_duplicate = await this.prisma.channel.findUnique({
+      where: {
+        channelName: CreateChannel.channelName
+      }
+    })
+
+    if (find_duplicate) {
+      return {
+        "status": 400,
+        "message": "Channel already exists"
+      }
+    }
+    console.log("result: ", find_duplicate);
     const create_channel = await this.prisma.channel.create({
       data: {
         channelName: CreateChannel.channelName,
