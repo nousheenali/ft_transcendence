c2f820a9ac2d17edd4b950f73c8e639109525dab
yonatan46 <yonatankefyalew65@gmail.com>
2023-10-22 18:00:02 +0400

    hidden sizebar for /game page

--- frontend/src/components/layout/index.tsx
+++ frontend/src/components/layout/index.tsx
@@ -1,17 +1,21 @@
 'use client';
+import { useRouter } from 'next/router';
 import MobileSidebar from '../mobileSidebar/mobileSideBar';
 import AsideBar from './asidebar';
+import { usePathname } from 'next/navigation';
 
 interface MainLayoutProps {
   children: React.ReactNode;
 }
 
 export default function MainLayout({ children }: MainLayoutProps) {
+  const path = usePathname();
+  const isGame = !!(path === '/game')
   return (
     <main>
       <MobileSidebar />
       <div className="flex w-full space-x-4 h-screen max-h-screen py-5 px-4">
-        <AsideBar isMobile={false} />
+        {!isGame && <AsideBar isMobile={false} />}
 
         <main className="w-full">{children}</main>
       </div>
