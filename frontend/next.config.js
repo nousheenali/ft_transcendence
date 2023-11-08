3efbe6bdeafbf432680d497d0595835599c786a3
Sumi111 <sumaiyasweet16@gmail.com>
2023-11-08 10:45:26 +0400

    TRAN-144 added domain in config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["cdn.intra.42.fr", "i.imgur.com"]
+        domains: ["cdn.intra.42.fr", "i.imgur.com", "localhost"]
     }
 }
 
