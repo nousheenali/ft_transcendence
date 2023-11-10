4a0e2f19061261c1bb65231ba8ce232351da3e2a
Ghaith Abdoush <ghaith181994@hotmail.com>
2023-11-10 13:51:35 +0400

    change rendring in the Channel component

--- frontend/src/components/Chat/Channels/ChannelsSideBar/ChannelsList/OneChannel.tsx
+++ frontend/src/components/Chat/Channels/ChannelsSideBar/ChannelsList/OneChannel.tsx
@@ -3,7 +3,6 @@
 import Image from "next/image";
 import React from "react";
 import { ChannelsProps } from "../../../types";
-import { MdOutlineGroupAdd, MdGroupRemove } from "react-icons/md";
 import { activateClickedChannel } from "@/context/store";
 export default function Channel({
   channel,
@@ -15,59 +14,89 @@ export default function Channel({
   // define the active channel and the function to set the active channel from the store context of zustand
   // The state of the active channel will change according to the channel that the user clicks on.
 
-  const { activeChannel, setActiveChannel } = activateClickedChannel();
+  const { setActiveChannel } = activateClickedChannel();
 
-  return (
-    <div className="flex flex-row justify-center items-center w-80 h-14 rounded-xl px-1 py-1 overflow-hidden">
-      <div
-        className="flex flex-row justify-center items-center w-80 h-14 rounded-xl px-1 py-1 overflow-hidden hover:cursor-pointer"
-        onClick={() => setActiveChannel(channel)}
-      >
-        {/* [1] Display the avatar of the channel */}
+  if (isJoined === false) {
+    return (
+      <div className="flex flex-row justify-center items-center w-80 h-14 rounded-xl px-1 py-1 overflow-hidden">
+        <div
+          className="flex flex-row justify-center items-center w-80 h-14 rounded-xl px-1 py-1 overflow-hidden hover:cursor-pointer"
+          onClick={() => setActiveChannel({} as ChannelsProps)}
+        >
+          {/* [1] Display the avatar of the channel */}
 
-        <div className="w-36 h-12 basis-1/6 overflow-hidden">
-          <Image
-            alt={channel.channelName}
-            src={"/chat/people.svg"}
-            width={37}
-            height={37}
-          />
+          <div className="w-36 h-12 basis-1/6 overflow-hidden">
+            <Image
+              alt={channel.channelName}
+              src={"/chat/people.svg"}
+              width={37}
+              height={37}
+            />
+          </div>
+
+          {/* [2] Display the name of the channel */}
+
+          <div className="flex flex-col overflow-y-hidden basis-4/6 ml-2">
+            <span className="font-saira-condensed text-main-text font-light truncate ...">
+              {channel.channelName}
+            </span>
+          </div>
         </div>
 
-        {/* [2] Display the name of the channel */}
+        {/* [3] If the user already joined dont show the join button */}
 
-        <div className="flex flex-col overflow-y-hidden basis-4/6 ml-2">
-          <span className="font-saira-condensed text-main-text font-light truncate ...">
-            {channel.channelName}
-          </span>
-        </div>
-      </div>
-
-      {/* [3] If the user already joined dont show the join button */}
-
-      {!isJoined ? (
         <div className="flex flex-row  gap-5 pr-2">
           <button className="flex flex-row items-center gap-1 text-dimmed-text font-thin">
             <Image
-              alt={"invite"}
+              alt={"join channel"}
               src={"./chat/user-cirlce-add.svg"}
               width={30}
               height={30}
             />
           </button>
         </div>
-      ) : (
+      </div>
+    );
+  } else {
+    return (
+      <div className="flex flex-row justify-center items-center w-80 h-14 rounded-xl px-1 py-1 overflow-hidden">
+        <div
+          className="flex flex-row justify-center items-center w-80 h-14 rounded-xl px-1 py-1 overflow-hidden hover:cursor-pointer"
+          onClick={() => setActiveChannel(channel)}
+        >
+          {/* [1] Display the avatar of the channel */}
+
+          <div className="w-36 h-12 basis-1/6 overflow-hidden">
+            <Image
+              alt={channel.channelName}
+              src={"/chat/people.svg"}
+              width={37}
+              height={37}
+            />
+          </div>
+
+          {/* [2] Display the name of the channel */}
+
+          <div className="flex flex-col overflow-y-hidden basis-4/6 ml-2">
+            <span className="font-saira-condensed text-main-text font-light truncate ...">
+              {channel.channelName}
+            </span>
+          </div>
+        </div>
+
+        {/* [3] If the user already joined dont show the join button */}
+
         <div className="flex flex-row  gap-5 pr-2">
           <button className="flex flex-row items-center gap-1 text-dimmed-text font-thin">
             <Image
-              alt={"invite"}
+              alt={"leave channel"}
               src={"./chat/Sign_out_circle_duotone_line.svg"}
               width={30}
               height={30}
             />
           </button>
         </div>
-      )}
-    </div>
-  );
+      </div>
+    );
+  }
 }
