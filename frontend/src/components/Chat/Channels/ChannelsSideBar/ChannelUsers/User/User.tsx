156ac926353bd48b1e16196c3986c3837a5d33a8
Ghaith Abdoush <ghaith181994@hotmail.com>
2023-11-16 07:07:38 +0400

    add non functional btns for mute and kick channel users

--- frontend/src/components/Chat/Channels/ChannelsSideBar/ChannelUsers/User/User.tsx
+++ frontend/src/components/Chat/Channels/ChannelsSideBar/ChannelUsers/User/User.tsx
@@ -1,15 +1,47 @@
-import { ChannelUserProps } from "../../../../types";
 import Image from "next/image";
+import React, { useState } from "react";
+import { BiVolumeMute } from "react-icons/bi";
+import { RiUserUnfollowLine } from "react-icons/ri";
+import { ChannelUserProps, ChannelsProps } from "../../../../types";
+import { userInformation } from "@/components/Profile/types";
 
-export default function ChannelUser(data: { user: ChannelUserProps }) {
+export default function ChannelUser({
+  currentUser,
+  user,
+  channel,
+}: {
+  currentUser: userInformation;
+  user: ChannelUserProps;
+  channel: ChannelsProps;
+}) {
+  const [muteColor, setMuteColor] = useState("gray");
+
+  const handleMuteClick = () => {
+    if (currentUser.login === user.login)
+      console.log("you can't mute yourself");
+    else if (currentUser.id === channel.createdBy) {
+      console.log("mute the user", user.name);
+      setMuteColor((prevColor) =>
+        prevColor === "gray" ? "rgba(213, 242, 35, 0.8)" : "gray"
+      );
+    }
+  };
+
+  const handleKickClick = () => {
+    if (currentUser.login === user.login)
+      console.log("you can't kick yourself");
+    else if (currentUser.id === channel.createdBy) {
+      console.log("kick the user", user.name);
+    }
+  };
+
   return (
-    <div className="flex flex-row justify-center items-center w-80 h-14 rounded-xl px-1 py-1 overflow-hidden hover:cursor-pointer">
+    <div className="flex flex-row justify-center items-center w-80 h-14 rounded-xl px-1 py-1 ml-6 overflow-hidden hover:cursor-pointer">
       <div className="indicator profile w-36 h-12 basis-1/6 rounded-3xl overflow-hidden relative">
-        <div
-          className="rounded-full w-[45px] h-[45px] overflow-hidden border-2 border-main-yellow">
-          <Image alt={data.user.name} src={data.user.avatar} width={45} height={45}  />
+        <div className="rounded-full w-[45px] h-[45px] overflow-hidden border-2 border-main-yellow">
+          <Image alt={user.name} src={user.avatar} width={45} height={45} />
         </div>
-        {data.user.isOnline ? (
+        {user.isOnline ? (
           <span className="indicator-item indicator-bottom badge bg-green-400 badge-xs absolute left-7 top-6"></span>
         ) : (
           <span className="indicator-item indicator-bottom badge bg-red-400 badge-xs absolute left-7 top-6"></span>
@@ -18,8 +50,35 @@ export default function ChannelUser(data: { user: ChannelUserProps }) {
 
       <div className="flex flex-col overflow-y-hidden basis-4/6 ml-2">
         <span className="font-saira-condensed text-main-text font-light truncate ...">
-          {data.user.name}
+          {user.name}
         </span>
+      </div>
+
+      {/* ====================================================== */}
+      <div className="flex flex-row gap-3">
+        <div onClick={handleMuteClick}>
+          <BiVolumeMute
+            className="text-main-text"
+            size={20}
+            color={muteColor}
+          />
+        </div>
+        <div onClick={handleKickClick}>
+          {currentUser.id === channel.createdBy &&
+          user.login !== currentUser.login ? (
+            <RiUserUnfollowLine
+              className="text-main-text"
+              size={20}
+              color={"#CD5C5C"}
+            />
+          ) : (
+            <RiUserUnfollowLine
+              className="text-main-text"
+              size={20}
+              color={"grey"}
+            />
+          )}
+        </div>
       </div>
     </div>
   );
--- frontend/src/components/Chat/Channels/ChannelsSideBar/ChannelUsers/UsersList/UsersList.tsx
+++ frontend/src/components/Chat/Channels/ChannelsSideBar/ChannelUsers/UsersList/UsersList.tsx
@@ -1,4 +1,3 @@
-
 import React, { useContext, useEffect, useState } from "react";
 import { activateClickedChannel } from "@/context/store";
 import { ChannelUserProps } from "@/components/Chat/types";
@@ -6,13 +5,16 @@ import { getChannelUsersData } from "../../../../../../../services/channels";
 import { API_ENDPOINTS } from "../../../../../../../config/apiEndpoints";
 import User from "@/components/Chat/Channels/ChannelsSideBar/ChannelUsers/User/User";
 import { AuthContext } from "@/context/AuthProvider";
+import { userInformation } from "@/components/Profile/types";
+import { getUserData } from "../../../../../../../services/user";
 
 //============================================================================================//
 export default function UsersList() {
-    const {user} = useContext(AuthContext)
+  const { user } = useContext(AuthContext);
   const [isLoading, setLoading] = useState(true);
   const { activeChannel } = activateClickedChannel();
   const [channelUsers, setChannelUsers] = useState<ChannelUserProps[]>([]);
+  const [currentUser, setCurrentUser] = useState<userInformation>();
 
   /**
    **╭── 🌼
@@ -21,13 +23,18 @@ export default function UsersList() {
    **/
 
   useEffect(() => {
-    if (activeChannel.channelName) {
+    if (user && user.login && activeChannel.channelName) {
       const fetchData = async () => {
         const users: ChannelUserProps[] = await getChannelUsersData(
-         user.login!,
+          user.login!,
           API_ENDPOINTS.channelUsers + activeChannel.channelName + "/"
         );
         setChannelUsers(users);
+        const currentUser: userInformation = await getUserData(
+          user.login!,
+          API_ENDPOINTS.getUserbyLogin
+        );
+        setCurrentUser(currentUser);
         setLoading(false);
       };
       fetchData();
@@ -39,7 +46,8 @@ export default function UsersList() {
    **├ 👇 Show the loading spinner while fetching the channel's users data
    **└── 🌼
    **/
-  if (activeChannel.channelName === undefined ) return <p>No channel selected</p>;
+  if (activeChannel.channelName === undefined)
+    return <p>No channel selected</p>;
 
   if (isLoading)
     return (
@@ -51,7 +59,12 @@ export default function UsersList() {
     <div className="flex flex-col w-full h-1/4 px-1 rounded-xl overflow-y-scroll scroll-container">
       {channelUsers.map((OneUser, index) => (
         <div key={index}>
-          <User key={index} user={OneUser} />
+          <User
+            key={index}
+            currentUser={currentUser!}
+            user={OneUser}
+            channel={activeChannel}
+          />
         </div>
       ))}
     </div>
