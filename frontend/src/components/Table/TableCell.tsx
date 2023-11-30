8037b33852dd2829744ef786ccef03c1a50b7b5e
Ghaith Abdoush <ghaith181994@hotmail.com>
2023-11-30 20:52:19 +0400

    Fix the score

--- frontend/src/components/Table/TableCell.tsx
+++ frontend/src/components/Table/TableCell.tsx
@@ -39,7 +39,7 @@ const TableCell: React.FC<TableCellProps> = ({
               alt="avatar"
             />
           </div>
-          <div>
+          <div className="text-left text-main-text font-bold">
             {dataItem.name?.length > 10
               ? `${dataItem.name.substring(0, 15)}..`
               : dataItem.name}
@@ -107,7 +107,7 @@ const TableCell: React.FC<TableCellProps> = ({
     }
   };
 
-  // if both of the above doesnt match , returns the cell styles to show the action icons
+  // if both of the above doesn't match , returns the cell styles to show the action icons
   return (
     <div className="py-2 flex-1 text-center">
       <div className="flex items-center justify-center flex-row hover:cursor-pointer">
